d3f7697e37efbbcfff0e8f64fd9873ae559e9e7c
eileendchen <eileendchen@gmail.com>
2024-01-30 23:16:42 -0500

    create campfire games section

--- components/CampfireGames.vue
+++ components/CampfireGames.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="campfire-games" class="section">
-    <h1 class="section-title">Campfire Games</h1>
-    <p id="cfg-blurb">
-      The Campfire Games is a way to learn, grow, and build with the Bitcamp
-      Community. At the start of this year's event, you will join one of three
-      teams based on your personality and interests &mdash; joining forces with
-      hackers from around the world! By winning unique challenges, attending
-      workshops, and participating in mini-events, you'll rack up points for
-      your team. At the end of Bitcamp, members of the winning team will receive
-      limited edition Bitcamp Apparel. Find your community, develop your team
-      identity, and collaborate on something bigger than yourself:
-      <br />#FindYourFrontier
-    </p>
+    <div id="sign-info">
+      <img
+        class="campfire-games"
+        src="../assets/images/campfire-games/cfg-sign.svg"
+        alt="Campfire Games"
+      />
+
+      <p id="cfg-blurb">
+        The Campfire Games is a way to learn, grow, and build with the Bitcamp
+        Community. At the start of this year's event, you will join one of three
+        teams based on your personality and interests &mdash; joining forces
+        with hackers from around the world! By winning unique challenges,
+        attending workshops, and participating in mini-events, you'll rack up
+        points for your team. At the end of Bitcamp, members of the winning team
+        will receive limited edition Bitcamp Apparel.
+        <br /><br />Find your community, develop your team identity, and
+        collaborate on something bigger than yourself: <br />#AllAboutTheJourney
+      </p>
+    </div>
 
     <div id="teams-div">
       <div class="team-section">
         <img
           class="team-picture red-marshie"
-          src="../assets/images/marshies/rider.png"
+          src="../assets/images/marshies/motorcyclist.svg"
           alt="Red marshie"
         />
         <h3 class="team-title red">Red Team</h3>
@@ -30,7 +37,7 @@
       <div class="team-section">
         <img
           class="team-picture"
-          src="../assets/images/marshies/hiker.png"
+          src="../assets/images/marshies/car-driver.svg"
           alt="Blue marshie"
         />
         <h3 class="team-title blue">Blue Team</h3>
@@ -44,7 +51,7 @@
       <div class="team-section">
         <img
           class="team-picture"
-          src="../assets/images/marshies/watcher.png"
+          src="../assets/images/marshies/van-driver.svg"
           alt="Green marshie"
         />
         <h3 class="team-title green">Green Team</h3>
@@ -63,7 +70,7 @@
 
 <style scoped lang="scss">
 #campfire-games {
-  width: 75%;
+  width: 85%;
   /* height: 300px; */
   box-sizing: border-box;
   align-items: center;
@@ -75,16 +82,24 @@
 }
 
 #cfg-blurb {
-  padding-bottom: 10px;
+  padding-bottom: 20px;
+  text-align: left;
+  padding-top: 4.5rem;
+  max-width: 900px;
+  padding-left: 30px;
 }
 
 #teams-div {
   display: flex;
   flex-wrap: wrap;
   gap: 2rem;
-  padding-top: 30px;
+  padding-top: 60px;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   justify-content: center;
+}
+
+#sign-info {
+  display: flex;
 }
 
 .section-title {
@@ -101,11 +116,13 @@
 }
 
 img {
-  height: 200px;
+  height: 400px;
   margin: auto;
   border-color: red;
-  border-width: 1px;
-  padding-bottom: 10px;
+  padding-right: 1rem;
+  padding-left: 1rem;
+  max-width: 500px;
+  width: 100%;
 }
 
 .team-title {
@@ -115,18 +132,28 @@ img {
 @media only screen and (max-width: 1000px) {
   .team-section {
     width: 100%;
+    padding-bottom: 5rem;
+  }
+
+  #sign-info {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  #cfg-blurb {
+    max-width: 100vw;
   }
 }
 
 .blue {
-  color: #70cefe;
+  color: #70b0d1;
 }
 
 .red {
-  color: #ff6f75;
+  color: #ff3e46;
 }
 
 .green {
-  color: #46ea79;
+  color: #58c77b;
 }
 </style>
