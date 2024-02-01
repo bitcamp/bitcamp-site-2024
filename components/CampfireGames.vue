eeb2392c489c63e04786ef2bbc1e91155e892b71
eileendchen <79885623+eileendchen@users.noreply.github.com>
2024-02-01 16:38:00 -0500

    create campfire games section (#17)
    * create campfire games section
    
    * resize marshies and add padding
    
    * Minor marshie sizing changes
    
    ---------

--- components/CampfireGames.vue
+++ components/CampfireGames.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="campfire-games" class="section">
-    <h1 class="section-title">Campfire Games</h1>
-    <p id="cfg-blurb">
-      The Campfire Games is a way to learn, grow, and build with the Bitcamp
-      Community. At the start of this year's event, you will join one of three
-      teams based on your personality and interests &mdash; joining forces with
-      hackers from around the world! By winning unique challenges, attending
-      workshops, and participating in mini-events, you'll rack up points for
-      your team. At the end of Bitcamp, members of the winning team will receive
-      limited edition Bitcamp Apparel. Find your community, develop your team
-      identity, and collaborate on something bigger than yourself:
-      <br />#FindYourFrontier
-    </p>
+    <div id="sign-info">
+      <img
+        class="campfire-games"
+        src="../assets/images/campfire-games/cfg-sign.svg"
+        alt="Campfire Games"
+      />
+
+      <p id="cfg-blurb">
+        The Campfire Games is a way to learn, grow, and build with the Bitcamp
+        Community. At the start of this year's event, you will join one of three
+        teams based on your personality and interests &mdash; joining forces
+        with hackers from around the world! By winning unique challenges,
+        attending workshops, and participating in mini-events, you'll rack up
+        points for your team. At the end of Bitcamp, members of the winning team
+        will receive limited edition Bitcamp Apparel.
+        <br /><br />Find your community, develop your team identity, and
+        collaborate on something bigger than yourself: <br />#AllAboutTheJourney
+      </p>
+    </div>
 
     <div id="teams-div">
       <div class="team-section">
         <img
           class="team-picture red-marshie"
-          src="../assets/images/marshies/rider.png"
+          src="../assets/images/marshies/motorcyclist.svg"
           alt="Red marshie"
         />
         <h3 class="team-title red">Red Team</h3>
@@ -30,7 +37,7 @@
       <div class="team-section">
         <img
           class="team-picture"
-          src="../assets/images/marshies/hiker.png"
+          src="../assets/images/marshies/car-driver.svg"
           alt="Blue marshie"
         />
         <h3 class="team-title blue">Blue Team</h3>
@@ -44,7 +51,7 @@
       <div class="team-section">
         <img
           class="team-picture"
-          src="../assets/images/marshies/watcher.png"
+          src="../assets/images/marshies/van-driver.svg"
           alt="Green marshie"
         />
         <h3 class="team-title green">Green Team</h3>
@@ -63,7 +70,7 @@
 
 <style scoped lang="scss">
 #campfire-games {
-  width: 75%;
+  width: 85%;
   /* height: 300px; */
   box-sizing: border-box;
   align-items: center;
@@ -72,19 +79,30 @@
   color: white;
   position: relative;
   margin-bottom: 10px;
+  padding-top: 100px;
 }
 
 #cfg-blurb {
-  padding-bottom: 10px;
+  padding-bottom: 20px;
+  text-align: left;
+  padding-top: 4.5rem;
+  max-width: 700px;
+  padding-right: 2vw;
+  padding-left: 70px;
+  color: black
 }
 
 #teams-div {
   display: flex;
   flex-wrap: wrap;
   gap: 2rem;
-  padding-top: 30px;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   justify-content: center;
+  padding-top: 10vh;
+}
+
+#sign-info {
+  display: flex;
 }
 
 .section-title {
@@ -101,32 +119,99 @@
 }
 
 img {
-  height: 200px;
+  height: 400px;
   margin: auto;
   border-color: red;
-  border-width: 1px;
-  padding-bottom: 10px;
+  padding-right: 1rem;
+  padding-left: 1rem;
+  max-width: 500px;
+  width: 90%;
 }
 
 .team-title {
   padding-top: 15px;
+  font-size: 1.5rem;
+}
+
+.team-picture {
+  max-width: 400px;
+}
+.team-picture.red-marshie {
+  width: 65%;
+}
+
+.team-blurb {
+  color: black;
+}
+
+@media screen and (min-width: 992px) and (max-width: 1200px) {
+  .team-picture {
+    width: 25vw;
+  }
+  .team-picture.red-marshie {
+    width: 19vw;
+  }
+}
+
+@media (min-width: 1900px) {
+  #cfg-blurb {
+    padding-right: 10vw;
+  }
+
+  .campfire-games {
+    margin-left: 20vw;
+    margin-right: 5vw;
+  }
 }
 
 @media only screen and (max-width: 1000px) {
   .team-section {
     width: 100%;
+    padding-bottom: 5rem;
+  }
+
+  #sign-info {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  #cfg-blurb {
+    max-width: 100vw;
+  }
+
+  .team-picture {
+    width: 60vw;
+  }
+
+  .team-picture.red-marshie {
+    width: 35vw;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .team-picture.red-marshie {
+    width: 50vw;
+  }
+}
+
+@media only screen and (max-width: 576px) {
+  .team-picture {
+    width: 70vw;
+  }
+  .team-picture.red-marshie {
+    width: 60vw;
   }
 }
 
 .blue {
-  color: #70cefe;
+  color: #70b0d1;
 }
 
 .red {
-  color: #ff6f75;
+  color: #ff3e46;
 }
 
 .green {
-  color: #46ea79;
+  color: #58c77b;
 }
 </style>
