8b00cc96718d602453cc1ebc7f70e99fde72a752
Joanne Lee <62729447+Joanne-Lee20@users.noreply.github.com>
2024-02-01 16:47:51 -0500

    Added New Tracks Information and Sign (#19)
    * Updated track descriptions and added tracks sign
    
    * update tracks sign position
    
    * update track icons and app dev track description

--- components/Tracks.vue
+++ components/Tracks.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="tracks" class="section">
-    <h1 class="tracks-title">Tracks</h1>
-
     <div class="tracks-container">
       <article class="card">
         <img
+          class="tracks-sign"
+          src="~/assets/images/tracks/tracks.svg"
+          alt="Tracks Sign Photo"
+        />
+      </article>
+      <article class="card">
+        <img
+          class="tracks-image"
           src="~/assets/images/tracks/general.svg"
           alt="General Track Photo"
         />
@@ -19,36 +25,24 @@
       </article>
       <article class="card">
         <img
-          src="~/assets/images/tracks/beginner.svg"
-          alt="Beginner Track Photo"
+          class="tracks-image"
+          src="~/assets/images/tracks/ml.svg"
+          alt="Machine Learning Track Photo"
         />
         <div class="text">
-          <h1>Beginner Track</h1>
+          <h1>Machine Learning Track</h1>
           <p>
-            New to hackathons? Don't know how to code? No worries! At Bitcamp,
-            you'll have access to beginner-friendly workshops, helpful mentors,
-            personalized one-on-one sessions, and resources to kickstart your
-            coding journey.
+            If you're wowed by recent AI advancements like ChatGPT, then this is
+            the track for you! We will expand upon specialized learning
+            experiences by hosting a series of workshops with content related to
+            building and hosting ML models, unsupervised learning, and
+            discussions of recent AI advancements.
           </p>
         </div>
       </article>
       <article class="card">
         <img
-          src="~/assets/images/tracks/hardware.svg"
-          alt="Hardware Track Photo"
-        />
-        <div class="text">
-          <h1>Hardware Track</h1>
-          <p>
-            Embark on your hardware journey at Bitcamp with access to exclusive
-            hardware materials, mentors, and workshops! We'll provide all the
-            resources needed to build your very own hardware project and master
-            hardware fundamentals.
-          </p>
-        </div>
-      </article>
-      <article class="card">
-        <img
+          class="tracks-image"
           src="~/assets/images/tracks/quantum.svg"
           alt="Quantum Track Photo"
         />
@@ -58,26 +52,45 @@
             Hackers will delve into the field of quantum computing with
             exclusive mentors, sponsors, and workshops! Hackers will use their
             knowledge of Python and other computing skills on educational and
-            interactive Quantum Track activities. If you’ve been a previous
+            interactive Quantum Track activities. If you've been a previous
             participant of the Quantum track, there will be new, challenging
             prompts for you to tackle!
           </p>
-          <br />
-          <strong>Quantum Track sponsored by IonQ</strong>
         </div>
       </article>
       <article class="card">
         <img
-          src="~/assets/images/tracks/ml.svg"
-          alt="Machine Learning Track Photo"
+          class="tracks-image"
+          src="~/assets/images/tracks/cybersecurity.svg"
+          alt="Cybersecurity Track Photo"
         />
         <div class="text">
-          <h1>Machine Learning Track</h1>
+          <h1>Cybersecurity Track</h1>
           <p>
-            Our newest track! If you’re wowed by recent AI advancements like
-            ChatGPT, this is the track for you! We will have ML-related
-            workshops, guest speakers, and activities (basic Python programming
-            skills highly recommended).
+            One of our newest tracks! Explore the realm of cybersecurity and
+            understand various aspects of the field through interactive
+            workshops that include discussions of web vulnerabilities,
+            real-world applications of combating security weaknesses, and
+            ethical decision-making.
+          </p>
+        </div>
+      </article>
+      <article class="card">
+        <img
+          class="tracks-image"
+          src="~/assets/images/tracks/app_dev.svg"
+          alt="App Dev Track Photo"
+        />
+        <div class="text">
+          <h1>App Development Track</h1>
+          <p>
+            Ever wondered how to turn your innovative app idea into a reality?
+            Ready to turn your concepts into cutting-edge applications? Join the
+            App Dev track - we'll introduce you to different aspects of
+            development including the software development life cycle,
+            development tools such as Flutter, and full-stack development
+            through access to exclusive workshops and mentors as you work on
+            your hack!
           </p>
         </div>
       </article>
@@ -93,14 +106,13 @@ export default {
 
 <style scoped lang="scss">
 #tracks {
+  padding-top: 20%;
+  padding-bottom: 5%;
+  background-color: #f6ebcc;
   position: relative;
   z-index: 10;
 }
 
-.tracks-title {
-  text-align: center;
-  margin-bottom: 2rem;
-}
 .tracks-container {
   display: flex;
   flex-wrap: wrap;
@@ -110,7 +122,12 @@ export default {
   margin: 0 auto;
 
   @media (min-width: 576px) and (max-width: 767.8px) {
+    padding-top: 90px;
     width: 90%;
+  }
+
+  @media (min-width: 768px) and (max-width: 1199.98px) {
+    padding-top: 100px;
   }
 
   @media (max-width: 576px) {
@@ -127,18 +144,29 @@ export default {
   margin: 15px;
   margin-bottom: 15px;
 
-  img {
+  .tracks-sign {
+    width: calc(140% - 5px);
+    margin-top: -150px;
+    margin-right: 40px;
+  }
+  .tracks-image {
+    width: 117px;
+    height: 118px;
     max-width: 100%;
     margin-bottom: 20px;
   }
 
   .text {
+    color: #000;
     margin: 5px;
+    font-size: 16px;
     flex: 1;
   }
 
   .text h1 {
+    color: #000;
     margin-bottom: 15px;
+    font-size: 24px;
   }
 
   @media (min-width: 576px) and (max-width: 767.8px) {
@@ -147,12 +175,22 @@ export default {
     .text {
       margin: 0px;
     }
+
+    .tracks-sign {
+      width: calc(120% - 5px);
+      margin-right: 0px;
+    }
   }
 
   @media (min-width: 768px) and (max-width: 1199.98px) {
     width: calc(50% - 30px);
     .text {
       margin: 5px;
+    }
+
+    .tracks-sign {
+      width: calc(130% - 5px);
+      min-width: 350px;
     }
   }
 
@@ -162,6 +200,11 @@ export default {
 
     .text {
       margin: 0px;
+    }
+
+    .tracks-sign {
+      width: 300px;
+      transform: translateX(0) translateY(-20%);
     }
   }
 }
