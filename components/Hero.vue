c8b0a355f2cb61bc0aaf8fcc3611b0e39d27a638
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-02-28 11:33:34 -0500

    Replace multiple webps with one svg (#27)
    * sign svgs
    
    * updated campfire sign to include animations
    
    * Revert customizations on campfire none svg
    
    * Minified campfire svg
    
    * Updated lockfile
    
    * freshman did well
    
    * Convert campfire games to .webp
    
    * finish
    
    * update sign
    
    * minify svg
    
    * tweak animation to match current
    
    ---------
    
    Co-authored-by: SumitNawathe <50125892+SumitNawathe@users.noreply.github.com>
    Co-authored-by: Sai Pranav Theerthala <sptheerthala@gmail.com>
    Co-authored-by: Eric Chen <ericchen314@gmail.com>

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,43 +2,32 @@
   <div class="hero-wrapper">
     <div class="container">
       <div class="column column-1">
-        <img :src="signSrc" class="svgStyle" alt="Bitcamp sign" />
+        <img :src="sign_empty" class="svgStyle" alt="Bitcamp sign" />
       </div>
       <div class="column column-2">
         <responsive-text v-if="!isMobile"></responsive-text>
         <!-- <Slideshow v-if="!isMobile" class="slide" /> -->
       </div>
     </div>
-    
+
     <div class="hero">
-      <img class="foilage-large" src="~/assets/images/background/heroFoilage.svg" />
+      <img
+        class="foilage-large"
+        src="~/assets/images/background/heroFoilage.svg"
+      />
     </div>
   </div>
-  <responsive-text v-if="isMobile" :style="{ backgroundColor: '#4A3859'}" :slideshow=true></responsive-text>
+  <responsive-text
+    v-if="isMobile"
+    :style="{ backgroundColor: '#4A3859' }"
+    :slideshow="true"
+  ></responsive-text>
 </template>
 
 <script setup lang="ts">
-import ResponsiveText from "./ResponsiveHeroText.vue"
+import ResponsiveText from './ResponsiveHeroText.vue';
 
-import sign_empty from '../assets/images/bitcamp-sign/sign_empty.webp';
-import sign_full from '../assets/images/bitcamp-sign/sign_full.webp';
-import sign_1 from '../assets/images/bitcamp-sign/sign_1.webp';
-import sign_2 from '../assets/images/bitcamp-sign/sign_2.webp';
-import sign_3 from '../assets/images/bitcamp-sign/sign_3.webp';
-import sign_4 from '../assets/images/bitcamp-sign/sign_4.webp';
-import sign_5 from '../assets/images/bitcamp-sign/sign_5.webp';
-import sign_6 from '../assets/images/bitcamp-sign/sign_6.webp';
-import sign_7 from '../assets/images/bitcamp-sign/sign_7.webp';
-import sign_8 from '../assets/images/bitcamp-sign/sign_8.webp';
-import sign_9 from '../assets/images/bitcamp-sign/sign_9.webp';
-import sign_10 from '../assets/images/bitcamp-sign/sign_10.webp';
-import sign_11 from '../assets/images/bitcamp-sign/sign_11.webp';
-import sign_12 from '../assets/images/bitcamp-sign/sign_12.webp';
-import sign_13 from '../assets/images/bitcamp-sign/sign_13.webp';
-import sign_14 from '../assets/images/bitcamp-sign/sign_14.webp';
-import sign_15 from '../assets/images/bitcamp-sign/sign_15.webp';
-import sign_16 from '../assets/images/bitcamp-sign/sign_16.webp';
-import sign_17 from '../assets/images/bitcamp-sign/sign_17.webp';
+import sign_empty from '../assets/images/bitcamp-sign/sign_full.svg';
 
 import { ref } from 'vue';
 
@@ -47,64 +36,36 @@ function incrementCounter() {
   counter.value++;
 }
 setInterval(incrementCounter, 250);
-
-let imagesList = [
-  sign_2,
-  sign_3,
-  sign_4,
-  sign_5,
-  sign_6,
-  sign_7,
-  sign_8,
-  sign_9,
-  sign_10,
-  sign_11,
-  sign_12,
-  sign_13,
-  sign_14,
-  sign_15,
-  sign_16,
-  sign_17,
-  sign_1,
-  sign_full,
-  sign_full,
-  sign_empty,
-  sign_empty,
-  sign_full,
-  sign_full,
-];
-
-const signSrc = computed(() => imagesList[counter.value % imagesList.length]);
 </script>
 
 <script lang="ts">
 export default {
   components: {
-    ResponsiveText
+    ResponsiveText,
   },
   data() {
     return {
-      screenWidth: 1000
+      screenWidth: 1000,
     };
   },
   computed: {
     isMobile() {
       return this.screenWidth <= 776;
-    }
+    },
   },
   mounted() {
     this.screenWidth = window.innerWidth;
     window.addEventListener('resize', this.updateScreenWidth);
   },
-  beforeDestroy() {
+  beforeUnmount() {
     window.removeEventListener('resize', this.updateScreenWidth);
   },
   methods: {
     updateScreenWidth() {
       this.screenWidth = window.innerWidth;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
@@ -118,7 +79,6 @@ export default {
 
   @media (max-width: 1000px) {
     padding-bottom: 7em;
-    
   }
   @media (max-width: 776px) {
     padding-bottom: 0em;
@@ -133,7 +93,6 @@ export default {
   width: 100%;
   height: 95%;
 
-
   @media (max-width: 1000px) {
     height: 80%;
   }
@@ -141,7 +100,6 @@ export default {
   @media (max-width: 776px) {
     height: 100%;
   }
-  
 }
 .space {
   height: 300px;
@@ -180,7 +138,7 @@ export default {
   width: 100%;
   margin-left: auto;
   margin-right: auto;
-  // display: block; 
+  // display: block;
   // position: absolute;
 }
 
@@ -195,7 +153,6 @@ export default {
 }
 
 @media (max-width: 776px) {
-  
   .column-2 {
     display: none;
   }
